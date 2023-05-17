edd939e8bb35d2188b02dbbec9b863cc38218966
Ahmed Osama <128548730+ahmed-osama10@users.noreply.github.com>
2023-05-17 10:53:27 +0300

    Update EncyclopediaImpl.java

--- EncyclopediaImpl.java
+++ EncyclopediaImpl.java
@@ -10,31 +10,41 @@ import java.rmi.server.UnicastRemoteObject;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+
 /**
- *
- * @author Ibrahim_Refa3i
+ * Implementation class for the Encyclopedia remote interface.
  */
 public class EncyclopediaImpl extends UnicastRemoteObject implements Encyclopedia {
     private static final long serialVersionUID = 1L;
     private final String data;
-    
+
+    /**
+     * Constructor for the Encyclopedia implementation.
+     * @param data The data representing the encyclopedia.
+     * @throws RemoteException If a remote communication error occurs.
+     */
     public EncyclopediaImpl(String data) throws RemoteException {
         super();
         this.data = data;
     }
 
     /**
-     *
-     * @return
-     * @throws RemoteException
+     * Calculates and returns the number of characters in the encyclopedia.
+     * @return The count of characters.
+     * @throws RemoteException If a remote communication error occurs.
      */
     @Override
     public int count() throws RemoteException {
         return data.length();
     }
-    
+
+    /**
+     * Retrieves an array of repeated words in the encyclopedia.
+     * @return An array of repeated words.
+     * @throws RemoteException If a remote communication error occurs.
+     */
     @Override
-     public String[] repeatedWords() throws RemoteException {
+    public String[] repeatedWords() throws RemoteException {
         String[] words = data.split("\\s+");
         Map<String, Integer> wordCounts = new HashMap<>();
         for (String word : words) {
@@ -49,33 +59,37 @@ public class EncyclopediaImpl extends UnicastRemoteObject implements Encyclopedi
                 .map(entry -> entry.getKey())
                 .toArray(String[]::new);
     }
-     
+
     /**
-     *
-     * @return
-     * @throws RemoteException
+     * Retrieves the longest word in the encyclopedia.
+     * @return The longest word.
+     * @throws RemoteException If a remote communication error occurs.
      */
     @Override
-     public String longest() throws RemoteException {
+    public String longest() throws RemoteException {
         String[] words = data.split("\\s+");
-        return Arrays.stream(words).max((a, b) -> a.length() - b.length()).orElse("");
+        return Arrays.stream(words)
+                .max((a, b) -> a.length() - b.length())
+                .orElse("");
     }
 
     /**
-     *
-     * @return
-     * @throws RemoteException
+     * Retrieves the shortest word in the encyclopedia.
+     * @return The shortest word.
+     * @throws RemoteException If a remote communication error occurs.
      */
     @Override
     public String shortest() throws RemoteException {
         String[] words = data.split("\\s+");
-        return Arrays.stream(words).min((a, b) -> a.length() - b.length()).orElse("");
+        return Arrays.stream(words)
+                .min((a, b) -> a.length() - b.length())
+                .orElse("");
     }
 
     /**
-     *
-     * @return
-     * @throws RemoteException
+     * Retrieves a map of word counts in the encyclopedia.
+     * @return A map of word counts.
+     * @throws RemoteException If a remote communication error occurs.
      */
     @Override
     public Map<String, Integer> repeat() throws RemoteException {
@@ -90,6 +104,4 @@ public class EncyclopediaImpl extends UnicastRemoteObject implements Encyclopedi
         }
         return wordCounts;
     }
-
-    
 }
