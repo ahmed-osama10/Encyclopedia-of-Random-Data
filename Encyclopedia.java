51427c8fce51ae482173f3409d739cc7254e0f3c
Ahmed Osama <128548730+ahmed-osama10@users.noreply.github.com>
2023-05-17 10:50:49 +0300

    Update Encyclopedia.java

--- Encyclopedia.java
+++ Encyclopedia.java
@@ -5,13 +5,21 @@ import java.rmi.RemoteException;
 import java.util.Map;
 
 public interface Encyclopedia extends Remote {
+    // Remote interface for the Encyclopedia
+
+    // Method to count the number of characters in the encyclopedia
     int count() throws RemoteException;
 
+    // Method to retrieve an array of repeated words in the encyclopedia
     String[] repeatedWords() throws RemoteException;
 
+    // Method to retrieve the longest word in the encyclopedia
     String longest() throws RemoteException;
 
+    // Method to retrieve the shortest word in the encyclopedia
     String shortest() throws RemoteException;
 
+    // Method to retrieve a map of word counts in the encyclopedia
     Map<String, Integer> repeat() throws RemoteException;
 }
+
